8d02e3a2dcf6ffc4f2cee78efe4af181f1ba4ea8
EkaterinaMarkina-1 <124836608+EkaterinaMarkina-1@users.noreply.github.com>
2024-01-11 23:16:39 +0600

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
 st.title('My first app')
-df_class = pd.read_csv("weatherAUS.csv")
-Region = st.multiselect('Choose regions', df_class['region'].unique())
-Smoker = st.multiselect('Choose smoker', df_class['smoker'].unique())
-Sex = st.radio('Choose a gender', df_class['sex'].unique())
-Age = st.slider('Choose age', min_value=17, max_value=66, value=(20, 50))
-Children = st.number_input('Choose the number of children', min_value= 0,
- max_value=5, value =1, step=1)
+df_class = pd.read_csv("mumbai_houses_task.csv")
+Location = st.multiselect('Choose location', df_class['location'].unique())
+Min_temp = st.radio('Choose a min_temp', df_class['min_temp'].unique())
+Max_temp = st.radio('Choose a max_temp', df_class['max_temp'].unique())
+Rainfall = st.radio('Choose a rainfall', df_class['rainfall'].unique())
+Evaporation = st.radio('Choose a evaporation', df_class['evaporation'].unique())
+Sunshine = st.radio('Choose a sunshine', df_class['sunshine'].unique())
+WindGustDir = st.radio('Choose a wind_gust_dir', df_class['wind_gust_dir'].unique())
+WindGustSpeed = st.radio('Choose a wind_gust_speed', df_class['wind_gust_speed'].unique())
+WindGustSpeed = st.radio('Choose a wind_gust_dir', df_class['wind_gust_dir'].unique())
+WindGustSpeed = st.radio('Choose a wind_gust_dir', df_class['wind_gust_dir'].unique())
+WindGustSpeed = st.radio('Choose a wind_gust_dir', df_class['wind_gust_dir'].unique())
+WindGustSpeed = st.radio('Choose a wind_gust_dir', df_class['wind_gust_dir'].unique())
+
+
+
+
+
+
 new_df = df_class[(df_class['region'].isin(Region)) & (df_class['smoker'].isin(Smoker)) & (df_class['sex'] =
 = Sex)
  & (df_class['age']> Age[0]) & (df_class['age']< Age[1])
