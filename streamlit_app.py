398c72c33676c28eba0a99cd9d70e1e4f1326ec1
EkaterinaMarkina-1 <124836608+EkaterinaMarkina-1@users.noreply.github.com>
2024-01-11 20:52:12 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,53 @@
 
+import streamlit as st
+
+# split the code into different functions for each page
+
+def main():
+
+    # option to choose between 
+    select_page = st.sidebar.selectbox("Prediction or About", ("Predict", "About"), key = "Select")
+    
+    # if user selects Predict page
+    if (select_page == "Predict"):
+        prediction_page()
+    
+    # if user selects About page
+    elif (select_page == "About"):
+        about()
+
+
+# First page - prediction page
+def prediction_page():
+    st.header("Interactive Diabetes Web Application")
+
+    Age = st.slider("Age", 0, 100)
+
+    st.write("Selected age is " + str(Age))
+
+    Question = st.selectbox("Select Answer", ("Yes", "No"), key = "answer")
+    if (Question == "Yes"):
+        st.write("You selected " + Question)
+
+    elif (Question == "No"):
+        st.write("You selected " + Question)
+
+    value = ''
+    checkbox_one = st.checkbox("Did Excercise")
+    checkbox_two = st.checkbox("Did not Exercise")
+
+    if (checkbox_one):
+      value = "Did Excercise"
+      
+    else:
+      value = "Did not Excercise"
+      
+    st.write(f"You {value}")
+    
+    
+# Second page - about page
+def about():
+    st.header("About this model")
+
+
+main()
