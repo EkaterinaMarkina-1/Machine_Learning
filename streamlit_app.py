366ae5eab83de9c0b23855042cd8418795e1d30d
EkaterinaMarkina-1 <124836608+EkaterinaMarkina-1@users.noreply.github.com>
2024-01-12 00:26:55 +0600

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,52 @@
 import streamlit as st
-import pandas as pd
-st.title('My first app')
-df_reg = pd.read_csv("mumbai_houses_task.csv")
 
-Prise = st.slider('Choose prise', min_value=0,
-                  max_value=1000000000000000, value=(20, 50))
-Area = st.slider('Choose area', min_value=0,
-                 max_value=10000000, value=(20, 50))
-Latitude = st.slider('Choose latitude', min_value=0,
-                     max_value=1000000000000000, value=(20, 50))
-Longitude = st.slider('Choose longitude', min_value=0,
-                      max_value=1000000000000000, value=(20, 50))
+# split the code into different functions for each page
 
-Bedrooms = st.multiselect('Choose bedrooms', df_reg['bedrooms'].unique())
-Bathrooms = st.multiselect('Choose bathrooms', df_reg['bathrooms'].unique())
-Balconyv = st.multiselect('Choose balconyv', df_reg['balconyv'].unique())
-Status = st.multiselect('Choose status', df_reg['status'].unique())
-Neworold = st.multiselect('Choose neworold', df_reg['neworold'].unique())
-Parking = st.multiselect('Choose parking', df_reg['parking'].unique())
-Furnished_status = st.multiselect(
-    'Choose furnished_status', df_reg['furnished_status'].unique())
-Lift = st.multiselect('Choose lift', df_reg['lift'].unique())
-Type_of_building = st.multiselect(
-    'Choose type_of_building', df_reg['type_of_building'].unique())
+def main():
+
+    # option to choose between 
+    select_page = st.sidebar.selectbox("Prediction or About", ("Predict", "About"), key = "Select")
+    
+    # if user selects Predict page
+    if (select_page == "Predict"):
+        prediction_page()
+    
+    # if user selects About page
+    elif (select_page == "About"):
+        about()
+
+
+# First page - prediction page
+def prediction_page():
+    st.header("Interactive Diabetes Web Application")
+
+    Age = st.slider("Age", 0, 100)
+
+    st.write("Selected age is " + str(Age))
+
+    Question = st.selectbox("Select Answer", ("Yes", "No"), key = "answer")
+    if (Question == "Yes"):
+        st.write("You selected " + Question)
+
+    elif (Question == "No"):
+        st.write("You selected " + Question)
+
+    value = ''
+    checkbox_one = st.checkbox("Did Excercise")
+    checkbox_two = st.checkbox("Did not Exercise")
+
+    if (checkbox_one):
+      value = "Did Excercise"
+      
+    else:
+      value = "Did not Excercise"
+      
+    st.write(f"You {value}")
+    
+    
+# Second page - about page
+def about():
+    st.header("About this model")
+
+
+main()
